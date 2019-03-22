32f6047ee418e6f274e7bc12f2e782b91ec0a00c
Russe1 <46652738+Russe1@users.noreply.github.com>
2019-03-22 19:41:01 +0300

    готовый вариант

--- js/java-script.js
+++ js/java-script.js
@@ -1,81 +1,80 @@
-    var letter = document.querySelector(".letter");
-    var map = document.querySelector(".big-map");
-    var buy = document.querySelectorAll(".button-buy");
-    var mark = document.querySelectorAll(".button-pack")
+//объявление переменных
 
-    //модальное окно отправки письма
-    if (letter) {
-        var popupLetter = document.querySelector(".modal-letter");
-        var closeLetter = popupLetter.querySelector("[name=modal-close]");
+var letter = document.querySelector(".letter");
+var map = document.querySelector(".big-map");
+var buy = document.querySelectorAll(".button-buy");
+var mark = document.querySelectorAll(".button-pack")
 
 
-        letter.addEventListener("click", function(evt) {
+//модальное окно покупок
+
+if (buy) {
+    var popupBuy = document.querySelector(".modal-buy");
+    var closeBuy = popupBuy.querySelector("[name=modal-close]");
+    var cart = document.querySelector(".cart")
+
+    for (var i = 0; i < buy.length; i++) {
+
+        buy[i].addEventListener("click", function(evt) {
             evt.preventDefault();
-            popupLetter.classList.add("modal-show");
-
+            popupBuy.classList.add("modal-show");
+            cart.classList.add("red");
         });
-        closeLetter.addEventListener("click", function(evt) {
+
+    };
+
+    closeBuy.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popupBuy.classList.remove("modal-show");
+    });
+}
+
+//добавление товара в закладки
+
+if (mark) {
+    var bookmark = document.querySelector(".bookmarks");
+
+    for (var i = 0; i < mark.length; i++) {
+
+        mark[i].addEventListener("click", function(evt) {
             evt.preventDefault();
-            popupLetter.classList.remove("modal-show");
+            bookmark.classList.add("on")
         });
+    };
+}
 
-    }
-
-    //модальное окно карты
-
-    if (map) {
-        var popupMap = document.querySelector(".modal-map");
-        var closeMap = popupMap.querySelector("[name=modal-close]");
-
-
-        map.addEventListener("click", function(evt) {
-            evt.preventDefault();
-            popupMap.classList.add("modal-show");
-
-        });
-        closeMap.addEventListener("click", function(evt) {
-            evt.preventDefault();
-            popupMap.classList.remove("modal-show");
-        });
-    }
+//модальное окно отправки письма
+if (letter) {
+    var popupLetter = document.querySelector(".modal-letter");
+    var closeLetter = popupLetter.querySelector("[name=modal-close]");
 
 
-    //модальное окно покупок
+    letter.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popupLetter.classList.add("modal-show");
 
-    if (buy) {
-        var popupBuy = document.querySelector(".modal-buy");
-        var closeBuy = popupBuy.querySelector("[name=modal-close]");
-        var cart = document.querySelector(".cart")
+    });
+    closeLetter.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popupLetter.classList.remove("modal-show");
+    });
+
+}
+
+//модальное окно карты
+
+if (map) {
+    var popupMap = document.querySelector(".modal-map");
+    var closeMap = popupMap.querySelector("[name=modal-close]");
 
 
+    map.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popupMap.classList.add("modal-show");
 
-        buy.forEach(function(element) {
-            element.addEventListener("click", function(evt) {
-                evt.preventDefault();
-                popupBuy.classList.add("modal-show");
-                cart.classList.add("red")
-
-
-            });
-
-        });
-
-        closeBuy.addEventListener("click", function(evt) {
-            evt.preventDefault();
-            popupBuy.classList.remove("modal-show");
-        });
-
-    }
-
-    //добавоение товара в закладки
-
-    if (mark) {
-        var bookmark = document.querySelector(".bookmarks");
-
-        mark.forEach(function(element) {
-            element.addEventListener("click",function(evt) {
-                evt.preventDefault();
-                bookmark.classList.add("on")
-            });
-        });
-    }
+    });
+    closeMap.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popupMap.classList.remove("modal-show");
+    });
+}
